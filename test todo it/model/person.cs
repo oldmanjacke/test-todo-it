ec2fd02bb918c941b015ba019706a8caf3fdd685
jacob jolsö <jacke_j_1998@hotmail.se>
2019-11-22 15:58:28 +0100

    lägger upp denna för kunna jobba hemma

--- test todo it/model/person.cs
+++ test todo it/model/person.cs
@@ -6,5 +6,14 @@ namespace test_todo_it.model
 {
     class person
     {
+        private readonly int personld;
+        private string firstName; 
+        private string lastName; 
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+
     }
 }
